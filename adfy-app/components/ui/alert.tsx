794a9823de3679f284e2737ad653b66f0045a808
SadiaIshtaq <sadiaishtaq21@gmail.com>
2025-08-04 18:23:03 +0530

    activity branch

--- adfy-app/components/ui/alert.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-function Alert({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div className={`aduffy-alert ${className}`} {...props} />
-  );
-}
-
-export { Alert };
